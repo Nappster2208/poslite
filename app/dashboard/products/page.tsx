b67fa12c445a61af7a1ab2c4ecb2fb08fb80edd2
phi <piamaulanaibrahim@gmail.com>
2023-12-16 10:24:30 +0700

    feat: adding component to product page

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <div>Product Page</div>;
+import { lusitana } from "@/app/ui/fonts";
+import { CreateProduct } from "@/app/ui/products/buttons";
+import Table from "@/app/ui/products/table";
+import Search from "@/app/ui/search";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Products",
+};
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page || 1);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Products</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search products..." />
+        <CreateProduct />
+      </div>
+      <Table query={query} currentPage={currentPage} />
+    </div>
+  );
 }
