1ab173af7a6d6f58840da577c79718fd337f2845
Nappster2208 <piamaulanaibrahim@gmail.com>
2023-12-17 20:36:40 +0700

    add nunito font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,10 @@
-import { Inter, Lusitana } from "next/font/google";
+import { Inter, Lusitana, Nunito } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
+
 export const lusitana = Lusitana({
   weight: ["400", "700"],
   subsets: ["latin"],
 });
+
+export const nuntio = Nunito({ subsets: ["latin"] });
