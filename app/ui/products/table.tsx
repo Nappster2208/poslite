4241a9d69b09d0e97f96b2feb27323e7a8c427f1
Nappster2208 <piamaulanaibrahim@gmail.com>
2023-12-17 20:37:21 +0700

    feat: Change label of Stock

--- app/ui/products/table.tsx
+++ app/ui/products/table.tsx
@@ -79,7 +79,7 @@ export default async function InvoicesTable({
                   Unit
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Stock
+                  Stock of goods
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
                   Price
