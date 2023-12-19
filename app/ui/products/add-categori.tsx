9d54effee91ea24e36adc3b919994471fcdd18b0
phi <piamaulanaibrahim@gmail.com>
2023-12-19 15:25:49 +0700

    Feat: Change to Select

--- app/ui/products/add-categori.tsx
+++ app/ui/products/add-categori.tsx
@@ -1,3 +1,5 @@
+import { TagIcon } from "@heroicons/react/20/solid";
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { ChangeEvent, useState } from "react";
 
 type MyObject = {
@@ -13,7 +15,7 @@ export function AddInput() {
     setVal([...val, { category: "" }]);
   };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>, index: number) => {
     const { value } = e.target;
     const list = [...val];
     list[index].category = value;
@@ -28,21 +30,33 @@ export function AddInput() {
   return (
     <div className="flex flex-col">
       {val.map((value, index) => (
-        <div key={index}>
-          <input
-            type="text"
-            name="myinput"
-            value={value.category}
-            onChange={(e) => handleChange(e, index)}
-          />
+        <div key={index} className="relative flex items-center">
+          <div className="flex w-full">
+            <select
+              id="productCategories"
+              name="productCategories"
+              aria-describedby="category-error"
+              defaultValue=""
+              onChange={(e) => handleChange(e, index)}
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+            >
+              <option value="" disabled>
+                Select a Category
+              </option>
+              <option value="Food">Food</option>
+              <option value="Nonfood">Non Food</option>
+              <option value="Clothes">Clothes</option>
+            </select>
+            <TagIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          </div>
           {val.length - 1 === index && setVal.length < 4 && (
             <button type="button" onClick={addHandle}>
-              +
+              <PlusIcon className="right-3 top-1/2 h-[18px] w-[18px] peer-focus:text-gray-900" />
             </button>
           )}
           {val.length > 1 && (
             <button type="button" onClick={() => removeHandle(index)}>
-              -
+              <MinusIcon className="right-3 top-1/2 h-[18px] w-[18px] peer-focus:text-gray-900" />
             </button>
           )}
         </div>
--- app/ui/products/create-form.tsx
+++ app/ui/products/create-form.tsx
@@ -143,7 +143,7 @@ export default function Form() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row flex-grow justify-between">
+          <div className="flex flex-col md:flex-row md:gap-4 flex-grow justify-between">
             {/* Product Categories */}
             <div className="mb-4 w-full">
               <label
@@ -153,24 +153,7 @@ export default function Form() {
                 Category
               </label>
               <div className="relative mt-2 rounded-md">
-                <div className="relative flex items-center">
-                  <select
-                    id="productCategories"
-                    name="productCategories"
-                    aria-describedby="category-error"
-                    defaultValue=""
-                    className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  >
-                    <option value="" disabled>
-                      Select a Category
-                    </option>
-                    <option value="PCS">Food</option>
-                    <option value="LSN">Non Food</option>
-                    <option value="BOX">Clothes</option>
-                  </select>
-                  <TagIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-                  <AddInput />
-                </div>
+                <AddInput />
               </div>
             </div>
 
