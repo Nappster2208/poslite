db5e42492439f538bb409fe328da91ff21787004
Alipia Maulana Ibrahim <40653791+Nappster2208@users.noreply.github.com>
2023-12-20 20:05:18 +0700

    Update create-form.tsx

--- app/ui/products/create-form.tsx
+++ app/ui/products/create-form.tsx
@@ -80,7 +80,7 @@ export default function Form() {
               <div className="flex flex-col items-center gap-4">
                 <div className="relative w-full max-w-md aspect-[70/45]">
                   <Image
-                    className="rounded-md shadow-md"
+                    className="rounded-md"
                     src="/1160358.png"
                     fill
                     sizes="(max-width: 768px) 100vw, 700px"
@@ -153,7 +153,23 @@ export default function Form() {
                 Category
               </label>
               <div className="relative mt-2 rounded-md">
-                <AddSelect />
+                <div className="flex w-full">
+                  <select
+                    id="productCategories"
+                    name="productCategories"
+                    aria-describedby="category-error"
+                    defaultValue=""
+                    className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  >
+                    <option value="" disabled>
+                      Select a Category
+                    </option>
+                    <option value="Food">Food</option>
+                    <option value="Nonfood">Non Food</option>
+                    <option value="Clothes">Clothes</option>
+                  </select>
+                  <TagIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                </div>
               </div>
             </div>
 
