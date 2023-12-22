6504b32d60e799186981d6a76829b9c13c7cfd15
Nappster2208 <piamaulanaibrahim@gmail.com>
2023-12-22 11:12:40 +0700

    feat: add submenu

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,49 +1,104 @@
 "use client";
 
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-  CurrencyDollarIcon,
-  ClipboardDocumentCheckIcon,
-  ClipboardDocumentListIcon,
-} from "@heroicons/react/24/outline";
+import React from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import {
+  HomeIcon,
+  CogIcon,
+  TagIcon,
+  ScaleIcon,
+  ClipboardDocumentListIcon,
+} from "@heroicons/react/24/outline"; // Change the icon imports as needed
+import { usePathname } from "next/navigation";
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
-const links = [
+interface NavLink {
+  name: string;
+  href: string;
+  icon: React.ElementType<any>; // Change this to React.ComponentType
+  hasSubmenu?: boolean; // Make hasSubmenu optional
+  submenu?: NavLink[];
+}
+
+const links: NavLink[] = [
   { name: "Home Page", href: "/dashboard", icon: HomeIcon },
   {
     name: "Products",
     href: "/dashboard/products",
     icon: ClipboardDocumentListIcon,
   },
-  { name: "Prices", href: "/dashboard/prices", icon: CurrencyDollarIcon },
+  {
+    name: "Reference",
+    href: "",
+    icon: CogIcon,
+    hasSubmenu: true,
+    submenu: [
+      {
+        name: "Categories",
+        href: "/dashboard/reference/categories",
+        icon: TagIcon,
+      },
+      {
+        name: "Units",
+        href: "/dashboard/reference/units",
+        icon: ScaleIcon,
+      },
+    ],
+  },
 ];
 
-export default function NavLinks() {
+const NavLinks: React.FC<{ items: NavLink[] }> = ({ items }) => {
   const pathname = usePathname();
+
   return (
     <>
-      {links.map((link) => {
+      {items.map((link) => {
         const LinkIcon = link.icon;
+
+        if (link.hasSubmenu) {
+          return (
+            <div key={link.name} className="relative group">
+              <Link legacyBehavior href={link.href}>
+                <a
+                  className={clsx(
+                    "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                    { "bg-sky-100 text-blue-600": pathname === link.href }
+                  )}
+                >
+                  <LinkIcon className="w-6" />
+                  <p className="hidden md:block">{link.name}</p>
+                </a>
+              </Link>
+
+              {link.hasSubmenu && (
+                <div className="absolute hidden group-hover:block mt-1 space-y-2">
+                  <NavLinks items={link.submenu || []} />
+                </div>
+              )}
+            </div>
+          );
+        }
+
         return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              { "bg-sky-100 text-blue-600": pathname === link.href }
-            )}
-          >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+          <Link legacyBehavior href={link.href} key={link.name}>
+            <a
+              className={clsx(
+                "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                { "bg-sky-100 text-blue-600": pathname === link.href }
+              )}
+            >
+              <LinkIcon className="w-6" />
+              <p className="hidden md:block">{link.name}</p>
+            </a>
           </Link>
         );
       })}
     </>
   );
-}
+};
+
+const Sidebar = () => {
+  return <NavLinks items={links} />;
+};
+
+export default Sidebar;
