763d85c31fb187261a1934c4e2b0ca33798e01e4
phi <piamaulanaibrahim@gmail.com>
2023-12-22 15:31:18 +0700

    feat: change create form style and add Purchase price

--- app/ui/products/create-form.tsx
+++ app/ui/products/create-form.tsx
@@ -172,9 +172,46 @@ export default function Form() {
               </div>
             </div>
           </div>
-
+        </div>
+        <div className="mt-2 bg-white shadow-md p-2 rounded-lg">
           <div className="grid">
             {/* Product Price */}
+            <div className="mb-4 lg:flex justify-between items-center">
+              <label
+                htmlFor="purchasePrice"
+                className="mb-2 block text-sm font-medium"
+              >
+                Purchase Price
+              </label>
+              <div className="relative">
+                <input
+                  id="purchasePrice"
+                  name="purchasePrice"
+                  type="number"
+                  className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  defaultValue=""
+                  aria-describedby="name-error"
+                ></input>
+                <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+              </div>
+              <label
+                htmlFor="margin"
+                className="mb-2 block text-sm font-medium"
+              >
+                Margin
+              </label>
+              <div className="relative">
+                <input
+                  id="margin"
+                  name="margin"
+                  type="number"
+                  className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  defaultValue=""
+                  aria-describedby="name-error"
+                ></input>
+                <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+              </div>
+            </div>
             <AddInput />
             {/* <input
                     id="productPrice"
