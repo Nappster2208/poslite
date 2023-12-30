c791440b5ed7e546074e45aceb69d5b0fdbc5f0e
phi <piamaulanaibrahim@gmail.com>
2023-12-30 11:54:26 +0700

    validation category

--- app/lib/action copy.ts
+++ app/lib/action copy.ts
@@ -2,22 +2,13 @@
 
 import connect from "./(connection)/connection";
 import m_categories from "./(models)/m_categories";
-import { z } from "zod";
-
-// Define Zod schema for formData
-const categorySchema = z.object({
-  catName: z.string().min(1).max(10), // Adjust the min and max values as needed
-  catDesc: z.string().min(1).max(100), // Adjust the min and max values as needed
-});
 
 export async function createCategory(formData: FormData) {
-  const data = categorySchema.parse({
+  const data = {
     catName: formData.get("catName"),
     catDesc: formData.get("catDesc"),
-  });
-
+  };
   console.log(data);
-
   try {
     await connect();
     await m_categories.create(data);
