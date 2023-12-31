1ef048c38aa72a8ee6b8efdb68b0b3579ef757c4
Nappster2208 <piamaulanaibrahim@gmail.com>
2023-12-31 23:04:38 +0700

    feat: add Edit category form ui

---
+++ app/dashboard/tools/categories/[id]/edit/page.tsx
@@ -0,0 +1,35 @@
+import { notFound } from "next/navigation";
+import { Metadata } from "next";
+import Breadcrumbs from "@/app/ui/breadcrumbs";
+import { FetchCategoryWithId } from "@/app/lib/data";
+import Form from "@/app/ui/tools/categories/edit-form";
+
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+};
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const data = await FetchCategoryWithId(id);
+  const category = JSON.parse(JSON.stringify(data));
+
+  if (!FetchCategoryWithId) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Categories", href: "/dashboard/tools/categories" },
+          {
+            label: "Edit Category",
+            href: `/dashboard/tools/categories/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form category={category} />
+    </main>
+  );
+}
