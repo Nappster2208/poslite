105d0ea44f249611e1a05f7a99852ab449ff6626
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-03 22:48:56 +0700

    backend: Add Menus API

--- app/api/menus/route.js
+++ app/api/menus/route.js
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 import connect from "@/app/lib/(connection)/connection";
 import m_categories from "@/app/lib/(models)/m_categories";
+import m_menu from "@/app/lib/(models)/m_menu";
 
 export const GET = async ({ request }) => {
   try {
     await connect();
-    const categories = await m_categories.find();
+    const categories = await m_menu.find();
     return new NextResponse(JSON.stringify(categories), { status: 200 });
   } catch (error) {
     return new NextResponse("Error in fetching Categories " + error, {
