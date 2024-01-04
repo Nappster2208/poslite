9bffcc7397b99a56bc062d6cc516c51b27f97466
phi <piamaulanaibrahim@gmail.com>
2024-01-04 15:22:24 +0700

    tes

--- app/ui/tools/categories/table.tsx
+++ app/ui/tools/categories/table.tsx
@@ -1,6 +1,5 @@
 import { FetchFilteredCategories } from "@/app/lib/data";
 import { AddSubCategory, DeleteCategory, UpdateCategory } from "./buttons";
-import React from "react";
 
 export default async function CategoryTable({
   query,
@@ -31,55 +30,26 @@ export default async function CategoryTable({
             </thead>
             <tbody className="bg-white">
               {categories?.map((category: any) => (
-                <React.Fragment key={category.id}>
-                  {/* Main Category Row */}
-                  <tr className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
-                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                      <div className="flex items-center gap-3">
-                        <p>{category.catName}</p>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-3">
-                      {category.catDesc}
-                    </td>
-                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                      <div className="flex justify-end gap-3">
-                        <AddSubCategory id={category.id} otherSub={[]} />
-                        <UpdateCategory id={category.id} />
-                        <DeleteCategory id={category.id} />
-                      </div>
-                    </td>
-                  </tr>
-
-                  {/* Subcategories Row */}
-                  {category.subCategory?.map((sub: any) => (
-                    <tr
-                      key={sub.id}
-                      className="w-full border-b py-3 text-sm last-of-type:border-none"
-                    >
-                      <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                        <div className="ml-10">
-                          <p>{sub.subcatName}</p>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-3">
-                        <div className="ml-10">
-                          <p>{sub.subcatDesc}</p>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                        <div className="flex justify-end gap-3">
-                          <AddSubCategory
-                            id={category.id}
-                            otherSub={[sub.subcatName]}
-                          />
-                          <UpdateCategory id={sub.id} />
-                          <DeleteCategory id={sub.id} />
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-                </React.Fragment>
+                <tr
+                  key={category.id}
+                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                >
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex items-center gap-3">
+                      <p>{category.catName}</p>
+                    </div>
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {category.catDesc}
+                  </td>
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex justify-end gap-3">
+                      <AddSubCategory id={category.id} otherSub={[]} />
+                      <UpdateCategory id={category.id} />
+                      <DeleteCategory id={category.id} />
+                    </div>
+                  </td>
+                </tr>
               ))}
             </tbody>
           </table>
