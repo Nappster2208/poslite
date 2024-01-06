54ade39bc8bce60837a54f1b053c2e84d37fd564
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-06 22:38:50 +0700

    delete the pagination feature on page

--- app/dashboard/tools/categories/subcategories/[id]/page.tsx
+++ app/dashboard/tools/categories/subcategories/[id]/page.tsx
@@ -1,39 +1,15 @@
 import { Metadata } from "next";
-import Search from "@/app/ui/search";
-import {
-  FetchFilteredSubCategories,
-  FetchSubCategoryPage,
-} from "@/app/lib/data";
 import Pagination from "@/app/ui/pagination";
-import React, { Suspense } from "react";
-import { TableSkeleton } from "@/app/ui/skeletons";
+import React from "react";
 import Breadcrumbs from "@/app/ui/breadcrumbs";
 import SubCategoryTable from "@/app/ui/tools/categories/subcategories/table";
-import { SubCategoryType } from "@/app/lib/types";
+import { FetchSubCategoryPage } from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: "Sub Categories",
 };
 
-type Subcategory = {
-  subcatName: string;
-  subcatDesc: string;
-  _id: string; // Ini bisa berupa string atau tipe lainnya tergantung implementasi ObjectId Anda
-};
-
-export default async function Page({
-  searchParams,
-  params,
-}: {
-  searchParams?: { query?: string; page?: string };
-  params: { id: string };
-}) {
-  const query = searchParams?.query || "";
-  const currentPage = Number(searchParams?.page || 1);
-  // const totalPages = await FetchSubCategoryPage(query, params.id);
-  const subCategories = await FetchFilteredSubCategories(params.id);
-
-  console.log(subCategories);
+export default async function Page({ params }: { params: { id: string } }) {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -48,14 +24,7 @@ export default async function Page({
           ]}
         />
       </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        {/* <Search placeholder="Search category..." /> */}
-        {/* <CreateCategory /> */}
-      </div>
-      <SubCategoryTable data={subCategories} />
-      <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
-      </div>
+      <SubCategoryTable id={params.id} />
     </div>
   );
 }
