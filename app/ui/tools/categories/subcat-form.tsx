a632d2aa97900164acb24770d4b99c0f66a0ceae
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-12 08:54:29 +0700

    import category schema type

--- app/ui/tools/categories/subcat-form.tsx
+++ app/ui/tools/categories/subcat-form.tsx
@@ -6,8 +6,6 @@ import { Buttons } from "../../button";
 import { lusitana } from "../../fonts";
 import clsx from "clsx";
 import {
-  categorySchema,
-  categorySchemaType,
   subcategorySchema,
   subcategorySchemaType,
 } from "@/validation/categorySchema";
@@ -39,13 +37,14 @@ const Subcat: React.FC<{ category: any }> = ({ category }) => {
   const onSubmit = async (data: subcategorySchemaType) => {
     try {
       // Call the createCategory function with the form data
-      await AddSubCategory(_id, {
-        subcatName: data.subcatName,
-        subcatDesc: data.subcatDesc,
+      await AddSubCategory({
+        catId: _id,
+        subcatName: data.Name,
+        subcatDesc: data.Description,
       });
       toast.success("Sub Category created successfully!");
     } catch (error) {
-      toast.success("Error updating sub category: " + error);
+      toast.error("Error updating sub category: " + error);
     }
   };
 
@@ -74,7 +73,7 @@ const Subcat: React.FC<{ category: any }> = ({ category }) => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="rounded-md p-4 md:p-6">
           <div className="bg-white shadow-md p-2 rounded-lg">
-            {/* Product Code */}
+            <input type="hidden" id="catId" value={_id} />
             <div className="mb-4">
               <label
                 htmlFor="subcatName"
@@ -87,11 +86,9 @@ const Subcat: React.FC<{ category: any }> = ({ category }) => {
                   id="subcatName"
                   className="peer block w-full cursor-text rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                   placeholder="Enter Category Name"
-                  {...register("subcatName")}
+                  {...register("Name")}
                 />
-                <span className="text-red-400">
-                  {errors.subcatName?.message}
-                </span>
+                <span className="text-red-400">{errors.Name?.message}</span>
                 <PencilIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
               </div>
             </div>
@@ -107,10 +104,10 @@ const Subcat: React.FC<{ category: any }> = ({ category }) => {
                   id="subcatDesc"
                   className="peer block w-full cursor-text rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                   placeholder="Enter Category Description"
-                  {...register("subcatDesc")}
+                  {...register("Description")}
                 />
                 <span className="text-red-400">
-                  {errors.subcatDesc?.message}
+                  {errors.Description?.message}
                 </span>
                 <PencilIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
               </div>
