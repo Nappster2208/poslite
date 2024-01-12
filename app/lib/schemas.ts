2c47f842ab3284c35122819d6b56e99795257bff
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-12 08:52:21 +0700

    create schemas

---
+++ app/lib/schemas.ts
@@ -0,0 +1,11 @@
+import * as yup from "yup";
+
+export const categorySchema = yup.object().shape({
+  catName: yup.string().min(3).max(20).required(),
+  catDesc: yup.string().min(3).max(100).required(),
+});
+export const subcategorySchema = yup.object().shape({
+  catId: yup.string(),
+  subcatName: yup.string().min(3).max(20).required(),
+  subcatDesc: yup.string().min(3).max(100).required(),
+});
