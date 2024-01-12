6aaa763666700bf8bca23b2998d33153422a7c3e
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-12 20:55:03 +0700

    create new page edit sub

--- app/dashboard/tools/subcategories/[id]/edit/page.tsx
+++ app/dashboard/tools/subcategories/[id]/edit/page.tsx
@@ -1,5 +1,30 @@
-const page = () => {
-  return <div>sub edit</div>;
+"use client";
+import Breadcrumbs from "@/app/ui/breadcrumbs";
+import { useSearchParams } from "next/navigation";
+
+const page = ({ params }: { params: { id: string } }) => {
+  const searchParams = useSearchParams();
+  const catId = searchParams.get("catid");
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Categories", href: "/dashboard/tools/categories" },
+          {
+            label: "Sub Category",
+            href: `/dashboard/tools/categories/${catId}/subcategories`,
+          },
+          {
+            label: "Edit",
+            href: "",
+            active: true,
+          },
+        ]}
+      />
+      {/* <Form /> */}
+    </main>
+  );
 };
 
 export default page;
