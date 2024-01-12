9bc329987472914dc8efbb3d777fde8a91daed8b
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-12 08:56:21 +0700

    change name yup validation

--- validation/categorySchema.ts
+++ validation/categorySchema.ts
@@ -2,8 +2,8 @@ import * as yup from "yup";
 
 export const categorySchema = yup
   .object({
-    catName: yup.string().min(3).max(20).required(),
-    catDesc: yup.string().min(3).max(100).required(),
+    Name: yup.string().min(3).max(20).required(),
+    Description: yup.string().min(3).max(100).required(),
   })
   .required();
 
@@ -11,8 +11,8 @@ export type categorySchemaType = yup.InferType<typeof categorySchema>;
 
 export const subcategorySchema = yup
   .object({
-    subcatName: yup.string().min(3).max(20).required(),
-    subcatDesc: yup.string().min(3).max(100).required(),
+    Name: yup.string().min(3).max(20).required(),
+    Description: yup.string().min(3).max(100).required(),
   })
   .required();
 
