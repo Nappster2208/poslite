16f4fa9bfeafba9453305117ce418ac3706304b7
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-12 08:51:57 +0700

    add join or aggregate and other

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,7 +1,7 @@
 import { unstable_noStore as noStore } from "next/cache";
 import m_categories from "./(models)/m_categories";
 import connect from "./(connection)/connection";
-import { NextResponse } from "next/server";
+import m_subCategories from "./(models)/m_subCategories";
 
 const ITEMS_PER_PAGE = 6;
 
@@ -14,16 +14,33 @@ export async function FetchFilteredCategories(
 
   try {
     await connect();
-    const categories = await m_categories
-      .find({
-        $or: [
-          { catName: { $regex: new RegExp(query, "i") } },
-          { catDesc: { $regex: new RegExp(query, "i") } },
-        ],
-      })
-      .sort({ createdAt: -1 })
-      .skip(offset)
-      .limit(ITEMS_PER_PAGE);
+    const categories = await m_categories.aggregate([
+      {
+        $match: {
+          $or: [
+            { catName: { $regex: new RegExp(query, "i") } },
+            { catDesc: { $regex: new RegExp(query, "i") } },
+          ],
+        },
+      },
+      {
+        $lookup: {
+          from: "r_subcategories",
+          localField: "_id",
+          foreignField: "catId",
+          as: "subCategories",
+        },
+      },
+      {
+        $sort: { createdAt: -1 },
+      },
+      {
+        $skip: offset,
+      },
+      {
+        $limit: ITEMS_PER_PAGE,
+      },
+    ]);
     return categories;
   } catch (error) {
     console.error("Database Error:", error);
@@ -56,15 +73,27 @@ export async function FetchCategoryPage(query: string) {
 }
 
 //Count Total Page of Sub Categories
+// export async function FetchSubCategoryPage(query: string, id: string) {
+//   noStore();
+//   try {
+//     await connect();
+//     const data = await m_categories.find({ _id: id });
+//     let totalPage = 1;
+//     data.map((item) => {
+//       totalPage = Math.ceil(item.subCategory.length / ITEMS_PER_PAGE);
+//     });
+//     return totalPage;
+//   } catch (error) {
+//     console.error("Database Error:", error);
+//     throw new Error("Failed to fetch total number of categories.");
+//   }
+// }
 export async function FetchSubCategoryPage(query: string, id: string) {
   noStore();
   try {
     await connect();
-    const data = await m_categories.find({ _id: id });
-    let totalPage = 1;
-    data.map((item) => {
-      totalPage = Math.ceil(item.subCategory.length / ITEMS_PER_PAGE);
-    });
+    const count = await m_subCategories.find({ catId: id }).countDocuments();
+    const totalPage = Math.ceil(count / ITEMS_PER_PAGE);
     return totalPage;
   } catch (error) {
     console.error("Database Error:", error);
@@ -72,18 +101,38 @@ export async function FetchSubCategoryPage(query: string, id: string) {
   }
 }
 
-export async function FetchFilteredSubCategories(id: string) {
+export async function FetchFilteredSubCategories(
+  id: string,
+  query: string,
+  currentPage: number
+) {
   noStore();
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
     await connect();
-    const data = await m_categories.findOne(
-      { _id: id },
-      { projection: { "subCategory._id": 0 } }
-    );
-    return data?.subCategory || [];
+    const subs = await m_subCategories
+      .find({
+        $or: [
+          { subcatName: { $regex: new RegExp(query, "i") } },
+          { subcatDesc: { $regex: new RegExp(query, "i") } },
+        ],
+      })
+      .sort({ createdAt: -1 })
+      .skip(offset)
+      .limit(ITEMS_PER_PAGE);
+    console.log(subs);
+    return subs;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch filtered categories.");
   }
+}
+
+export async function FetchSubCategoryWithId(id: string) {
+  try {
+    await connect();
+    const result = await m_subCategories.findById({ id });
+    return result;
+  } catch (error) {}
 }
