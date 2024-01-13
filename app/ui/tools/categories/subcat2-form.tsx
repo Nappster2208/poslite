4ebd4a58e71a70242b9825ed33e42c76d752bd28
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-13 22:50:06 +0700

    create subcat2 form

---
+++ app/ui/tools/categories/subcat2-form.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const Form = () => {
+  const catId = useSearchParams().get("sub");
+  const [catName, setCatName] = useState("");
+
+  useEffect(() => {
+    fetch(`/api/categories/getname/${catId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCatName(data);
+      });
+  }, [catId]);
+
+  return <div>{catName}</div>;
+};
+
+export default Form;
