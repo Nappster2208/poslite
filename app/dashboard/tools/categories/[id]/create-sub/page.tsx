57690fbe28e6872b00e47d78152ca45a8ed7cc2c
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-13 22:48:10 +0700

    change directory name

---
+++ app/dashboard/tools/categories/[id]/create-sub/page.tsx
@@ -0,0 +1,35 @@
+import { FetchCategoryWithId } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/breadcrumbs";
+import { notFound } from "next/navigation";
+import Form from "@/app/ui/tools/categories/subcat-form";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  const data = await FetchCategoryWithId(id);
+
+  const category = JSON.parse(JSON.stringify(data));
+
+  if (!FetchCategoryWithId) {
+    notFound();
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Categories", href: "/dashboard/tools/categories" },
+          {
+            label: "Sub Category",
+            href: `/dashboard/tools/categories/${id}/subcategories`,
+          },
+          {
+            label: "Add Sub Category",
+            href: ``,
+            active: true,
+          },
+        ]}
+      />
+      <Form category={category} />
+    </main>
+  );
+}
