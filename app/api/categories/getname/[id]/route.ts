2170efa82373242788cb8128b636c43dc603a5c1
Nappster2208 <piamaulanaibrahim@gmail.com>
2024-01-13 22:47:35 +0700

    create route getname categories

---
+++ app/api/categories/getname/[id]/route.ts
@@ -0,0 +1,18 @@
+import connect from "@/app/lib/(connection)/connection";
+import m_categories from "@/app/lib/(models)/m_categories";
+import { NextResponse } from "next/server";
+
+export const GET = async (request: Request) => {
+  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+  try {
+    await connect();
+    const data = await m_categories.findOne({ _id: id });
+    return new NextResponse(JSON.stringify(data?.catName || null), {
+      status: 200,
+    });
+  } catch (error) {
+    return new NextResponse("Error in fetching Sub Categories " + error, {
+      status: 500,
+    });
+  }
+};
