ece4ef7e6552ca48b2d464712b9e93cab0dc9492
phi <piamaulanaibrahim@gmail.com>
2024-01-15 10:24:52 +0700

    add revalidatePath

--- app/api/subs/getwithid/[id]/route.ts
+++ app/api/subs/getwithid/[id]/route.ts
@@ -1,11 +1,13 @@
 import connect from "@/app/lib/(connection)/connection";
 import m_subCategories from "@/app/lib/(models)/m_subCategories";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 export const GET = async (request: Request) => {
   const id = request.url.slice(request.url.lastIndexOf("/") + 1);
   try {
     await connect();
     const data = await m_subCategories.findOne({ _id: id });
+    revalidatePath(`/api/subs/getwithid/${id}`);
     return new NextResponse(JSON.stringify(data), {
       status: 200,
     });
