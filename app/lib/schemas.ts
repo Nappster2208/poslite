1a923509e08f0b90deab9657e6ccfac6757702c1
phi <piamaulanaibrahim@gmail.com>
2024-01-15 14:55:39 +0700

    add subcategoryschema

--- app/lib/schemas.ts
+++ app/lib/schemas.ts
@@ -9,3 +9,8 @@ export const subcategorySchema = yup.object().shape({
   subcatName: yup.string().min(3).max(20).required(),
   subcatDesc: yup.string().min(3).max(100).required(),
 });
+export const subcategory2Schema = yup.object().shape({
+  subcatId: yup.string(),
+  subcatName: yup.string().min(3).max(20).required(),
+  subcatDesc: yup.string().min(3).max(100).required(),
+});
