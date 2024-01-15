f436d9a756b3b9a96e7dfcc53246b2ba0ca73e35
phi <piamaulanaibrahim@gmail.com>
2024-01-15 10:29:57 +0700

    add revalidate path

--- app/api/categories/getname/[id]/route.ts
+++ app/api/categories/getname/[id]/route.ts
@@ -1,5 +1,6 @@
 import connect from "@/app/lib/(connection)/connection";
 import m_categories from "@/app/lib/(models)/m_categories";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export const GET = async (request: Request) => {
@@ -7,6 +8,7 @@ export const GET = async (request: Request) => {
   try {
     await connect();
     const data = await m_categories.findOne({ _id: id });
+    revalidatePath(`/api/categories/getname/${id}`);
     return new NextResponse(JSON.stringify(data?.catName || null), {
       status: 200,
     });
