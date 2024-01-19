a0000f6cd46b4aa08a7312c4bccdd793da38dbf6
phi <piamaulanaibrahim@gmail.com>
2024-01-19 15:28:37 +0700

    feat: create supplier page

---
+++ app/dashboard/master/supplier/page.tsx
@@ -0,0 +1,33 @@
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Supplier",
+};
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page || 1);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Supplier</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search supplier..." />
+        {/* <CreateProduct /> */}
+      </div>
+      {/* <Table query={query} currentPage={currentPage} /> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
+}
