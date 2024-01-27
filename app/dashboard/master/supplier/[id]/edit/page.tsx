78775a7484693353868ee87efd8699e82e834d23
phi <piamaulanaibrahim@gmail.com>
2024-01-27 11:23:43 +0700

    add edit form

--- app/dashboard/master/supplier/[id]/edit/page.tsx
+++ app/dashboard/master/supplier/[id]/edit/page.tsx
@@ -1,13 +1,20 @@
+import { FetchSupplierWithId } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/breadcrumbs";
+import Form from "@/app/ui/master/supplier/edit-form";
 
-const Page = () => {
+const Page = async ({ params }: { params: { id: string } }) => {
+  const supplier = await FetchSupplierWithId(params.id);
   return (
-    <Breadcrumbs
-      breadcrumbs={[
-        { label: "Supplier", href: "/dashboard/master/supplier/" },
-        { label: "Edit", href: "", active: true },
-      ]}
-    />
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Supplier", href: "/dashboard/master/supplier/" },
+          { label: "Edit", href: "", active: true },
+        ]}
+      />
+
+      <Form supplier={supplier} />
+    </main>
   );
 };
 
